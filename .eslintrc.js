a6a67ea0498fee82ddabac88096e045626da57ee
cuitl <spacesky214@sina.com>
2022-02-12 23:01:20 +0800

    chore: eslint add

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  globals: {
+    MSW_MOCK: 'writable',
+    JSX: 'readonly',
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': ['warn'],
+  },
+}
